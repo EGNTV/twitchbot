5f681a9e979b96c6c99b69168bf800450948bb71
Carl Boisvert <carlboisvert2012@gmail.com>
2017-02-16 12:40:54 -0500

    first commit

---
+++ bot.py
@@ -0,0 +1,59 @@
+# bot.py
+import cfg
+import socket
+import re
+import time
+
+
+def chat(sock, msg):
+    """
+    Send a chat message to the server.
+    Keyword arguments:
+    sock -- the socket over which to send the message
+    msg  -- the message to be sent
+    """
+    bytes = sock.send("WHO {} :{}".format(cfg.CHAN, msg).encode("utf-8"))
+    print(bytes);
+
+def ban(sock, user):
+    """
+    Ban a user from the current channel.
+    Keyword arguments:
+    sock -- the socket over which to send the ban command
+    user -- the user to be banned
+    """
+    chat(sock, ".ban {}".format(user))
+
+def timeout(sock, user, secs=600):
+    """
+    Time out a user for a set period of time.
+    Keyword arguments:
+    sock -- the socket over which to send the timeout command
+    user -- the user to be timed out
+    secs -- the length of the timeout in seconds (default 600)
+    """
+    chat(sock, ".timeout {}".format(user, secs))
+
+
+
+s = socket.socket()
+s.connect((cfg.HOST, cfg.PORT))
+s.send("PASS {}\r\n".format(cfg.PASS).encode("utf-8"))
+s.send("NICK {}\r\n".format(cfg.NICK).encode("utf-8"))
+s.send("JOIN {}\r\n".format(cfg.CHAN).encode("utf-8"))
+
+CHAT_MSG=re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+
+while True:
+    response = s.recv(1024).decode("utf-8")
+    print(response)
+    if response == "PING :tmi.twitch.tv\r\n":
+        s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+    else:
+        username = re.search(r"\w+", response).group(0) # return the entire match
+        message = CHAT_MSG.sub("", response)
+        print(username + ": " + message)
+        #if(message == "!score"):
+        print('Yes')
+        chat(s,"Message du IRC")
+    time.sleep(1 / cfg.RATE)
---
+++ cfg.py
@@ -0,0 +1,7 @@
+# cfg.py
+HOST = "irc.chat.twitch.tv"              # the Twitch IRC server
+PORT = 6667                         # always use port 6667!
+NICK = "snappydue"            # your Twitch username, lowercase
+PASS = "oauth:r3lhfgar9bkp61gqpfqzpgmf88m1a6" # your Twitch OAuth token
+CHAN = "#snappydue"                   # the channel you want to join
+RATE = (20/30) # messages per second
