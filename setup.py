bf232f8a21d56f0e13f3f4c5b89fbead5c88d89c
Carl Boisvert <carlboisvert@Carls-MBP.zyxel.com>
2017-02-18 13:56:11 -0500

    Setup basic environnement

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="TwitchBot",
+    # packages=[],
+    install_requires=[
+        "appdirs==1.4.0",
+        "et-xmlfile==1.0.1",
+        "jdcal==1.3",
+        "openpyxl==2.4.2",
+        "packaging==16.8",
+        "pyparsing==2.1.10",
+        "six==1.10.0"
+    ]
+)
